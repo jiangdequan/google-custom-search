4e15139782b7905e0f87da735f38eee43568641a
jiangdequan <jiangdequan1314@gmail.com>
2018-04-11 22:23:01 +0800

    init codes

---
+++ public/javascripts/index.js
@@ -0,0 +1,23 @@
+$(function () {
+
+    var public_key = "-----BEGIN PUBLIC KEY-----\n" +
+        "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC2s1jNEI45HuQGdIaNlrxi+GQp\n" +
+        "rZrzVEtxWD1Nq3s4899qkJIiFQr+MYRwCfRWvYn/xLRt/i1UqbtUvjkYOkyqFHu1\n" +
+        "svxZMAbGIUQqdFDkAIQcVYt5Ux2aUH/DJvBRsIF68TO7XTvImi6flJtle92rIsev\n" +
+        "qH+4kv82Txfa68nWnQIDAQAB\n" +
+        "-----END PUBLIC KEY-----";
+
+    var crypt = new JSEncrypt();
+    crypt.setPublicKey(public_key);
+
+    $('#search-button').click(function () {
+        var keywords = $('#search-text').val();
+
+        if (keywords === '' || keywords === undefined || keywords === null) {
+            return;
+        }
+
+        var encrypted = crypt.encrypt(keywords);
+        window.location.href = '/search?id=' + encodeURIComponent(encrypted);
+    });
+});
---
+++ routes/search.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var JSEncrypt = require('node-jsencrypt');
+var request = require('request');
+var Logger = require('../lib/log/logger');
+var FileUtils = require('../lib/util/fileutils');
+var Paging = require('../lib/util/paging');
+
+var config = FileUtils.loadJsonFile(__dirname + '/../config.json');
+
+// 私钥
+var private_key = process.env['PRIVATE_KEY'] || FileUtils.loadFile(__dirname + '/../private_key,perm');
+
+var decrypt = new JSEncrypt();
+decrypt.setPrivateKey(private_key);
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    // var body = FileUtils.loadJsonFile(__dirname + '/../test/result.json');
+    var text = decodeURIComponent(req.query.id);
+    Logger.debug(text, __filename);
+    var uncrypt = decrypt.decrypt(text);
+    Logger.debug(uncrypt, __filename);
+
+    var start = req.query.start;
+    var currentPageIndex = 1;
+    if (null === start || undefined === start || '' === undefined) {
+        start = 0;
+    } else {
+        currentPageIndex = Math.floor(start / config.num) + 1;
+    }
+    Logger.debug(currentPageIndex, __filename);
+
+
+    var url = FileUtils.spliceUrl(config, start) + uncrypt;
+    Logger.debug(url, __filename);
+
+    request(url, function (err, response, body) {
+        if (!err && response.statusCode == 200) {
+            var para = JSON.parse(body);
+            if (para.error) {
+                res.render('index');
+            } else {
+                var paging = Paging.paging(body.searchInformation.totalResults, config.num, currentPageIndex);
+                var para = body;
+                para.paging = paging;
+                para.q = req.query.id;
+                res.render('search', {title: uncrypt + ' - Google 搜索', cursor: para});
+            }
+        } else {
+            Logger.debug(err, __filename);
+            res.render('index', {title: 'Google'});
+        }
+    });
+});
+
+module.exports = router;
